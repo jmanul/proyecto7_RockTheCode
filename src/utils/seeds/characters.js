2ec417eb55ad99836a3a1f3e9d65a0dc7c00ab40
José Manuel Sánchez <jmanul77@gmail.com>
2024-09-06 01:39:43 +0200

    seeds

--- src/utils/seeds/characters.js
+++ src/utils/seeds/characters.js
@@ -13,10 +13,10 @@ const upSeed = async () => {
           await mongoose.connect("mongodb+srv://jmanul77:7HUBB2D5pNbCBjee@cluster0.s7uco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
           await Character.collection.drop();
-          console.log("delete seasons");
+          console.log("delete characters");
 
           await Character.insertMany(characters);
-          console.log("insert seasons");
+          console.log("insert characters");
 
           await mongoose.disconnect();
           console.log("disconect");
